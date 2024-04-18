2544fa106584685d8ba0a55661c1068db12de374
SaipakhooHooia <kawamotoiscute@gmail.com>
2024-04-18 14:41:52 +0800

    V0.3

--- homework_week3/crawl.py
+++ homework_week3/crawl.py
@@ -4,10 +4,6 @@ import json
 def crawl(url):
     response = urllib.request.urlopen(url)
     data = json.loads(response.read().decode('utf-8'))
-    filename = url.split('/')[-1]
-    if not filename.endswith('.json'):
-            filename += '.json'
-    with open(filename, 'w') as file:
-            file.write(json.dumps(data))
-    return (filename)
+    
+    return (data)
 #crawl('https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-2')
