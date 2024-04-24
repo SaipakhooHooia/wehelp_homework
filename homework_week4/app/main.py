85bf7dfff15f8a031eb42fdfb229f8ad624c41f5
SaipakhooHooia <kawamotoiscute@gmail.com>
2024-04-25 03:30:00 +0800

    V0.4

--- homework_week4/app/main.py
+++ homework_week4/app/main.py
@@ -22,7 +22,7 @@ login_data = {
 
 class ErrorType(str):
     empty_message="Please enter username and password",
-    wrong_message="Username orpassword is not correct"
+    wrong_message="Username or password is not correct"
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
@@ -33,6 +33,7 @@ async def read_item(request: Request):
 async def login(request: Request,username: Optional[str] = Form(None), password: Optional[str] = Form(None) ,remember_me: Optional[bool] = Form(None)):
     if username == login_data["username"] and password == login_data["password"] and remember_me:
         request.session["logged_in"] = True
+        request.session.pop("error_message", None)
         print("Session:", request.session)
         #return RedirectResponse(url="/member")
         #return request.session["logged_in"] == True
@@ -41,21 +42,29 @@ async def login(request: Request,username: Optional[str] = Form(None), password:
     elif not username or not password:
         error_message = "Please enter username and password"  # 自定義錯誤訊息
         request.session["error_message"] = error_message
+        #request.session["logged_in"] = False
         return JSONResponse(content={"error": error_message})
     elif username != login_data["username"] or password != login_data["password"]:
         error_message = "Username or password is not correct"  # 自定義錯誤訊息
         request.session["error_message"] = error_message
+        #request.session["logged_in"] = False
         return JSONResponse(content={"error": error_message})
     
 @app.get("/member", response_class=HTMLResponse)  
 async def login_success(request: Request):
     print("Session:", request.session)
     if request.session.get("logged_in") == True:
-        return templates.TemplateResponse("login_sucess.html", {"request": request})
-    if request.session.get("logged_in") == False:
+    #if request.session["logged_in"] == True:
+        #清除緩存，讓登入頁面不要被存取，避免按上一頁就可以返回/member
+        response = templates.TemplateResponse("login_sucess.html", {"request": request})
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Pragma"] = "no-cache"
+        response.headers["Expires"] = "0" 
+        return response
+    #if request.session.get("logged_in") == False:
+    else:
         return RedirectResponse(url="/")
     #else:
-    #    return RedirectResponse(url="/")
         #return templates.TemplateResponse("signin.html", {"request": request})
 
 @app.get("/error_dealing", response_class=HTMLResponse)  
@@ -66,7 +75,7 @@ async def login_fail(request: Request):
 @app.get("/error", response_class=HTMLResponse)  
 async def login_fail(request: Request):
     error_message = request.session.get("error_message")
-    print("error_message=", error_message)
+    #print("error_message=", error_message)
     
     return templates.TemplateResponse("login_fail.html", {"request": request, "message": error_message})
     
@@ -76,6 +85,8 @@ async def login_fail(request: Request):
 async def logout(request: Request):
     # 删除session数据
     request.session["logged_in"] = False
+    #request.session.pop("logged_in", None)
+    #request.session.clear()
     print("Session:", request.session)
     return RedirectResponse(url="/")
 
